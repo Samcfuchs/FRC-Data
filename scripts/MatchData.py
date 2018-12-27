bd7dae4aa2c58e74981e2d39426f2d2cb06529dc
Sam Fuchs <samcfuchs@gmail.com>
2018-12-27 11:45:18 -0500

    Add generic MatchData script

---
+++ scripts/MatchData.py
@@ -0,0 +1,59 @@
+import sys
+import importlib
+
+lib = importlib.import_module("Lib")
+
+# Get year
+try:
+    YEAR = sys.argv[1]
+except IndexError:
+    #YEAR = 2018
+    YEAR = input("Year (e.g. 2018): ")
+
+YEAR = str(YEAR)
+FILENAME = 'data/{}_MatchData_gen.csv'.format(YEAR)
+
+s = lib.s
+
+# define trim_score_breakdown
+trim_score_breakdown = lib.breakdown_trimmers[YEAR]
+
+
+print("Getting TBA data")
+matches, eventDetails = lib.get_data(YEAR)
+
+print("Removing bad matches")
+matches = lib.remove_empty_matches(matches)
+
+print("Imported {n} matches".format(n=len(matches)))
+
+print("Writing file")
+f = open(FILENAME, 'w')
+
+print("Writing header")
+f.write(','.join(lib.headers[YEAR]) + "\n")
+
+print("Writing data")
+for match in matches:
+    for alliance in match['alliances']:
+        robotnumber = 1
+        for team in match['alliances'][alliance]['team_keys']:
+            f.write(lib.get_event_data(match, eventDetails))
+
+            # Team/alliance stuff
+            f.write(team[3:] + ',')
+            f.write(alliance + ',')
+            f.write(str(robotnumber) + ',')
+
+            bd = trim_score_breakdown(robotnumber, match['score_breakdown'][alliance])
+            f.write(','.join(map(str, bd.values())) + ',')
+
+            # Record results for this team
+            f.write(lib.get_full_result(match,alliance))
+
+            f.write("\n")
+
+            robotnumber += 1 # Increment robot number to move on to next team in alliance
+
+f.close()
+print("Wrote data to {fname}".format(fname=FILENAME))
