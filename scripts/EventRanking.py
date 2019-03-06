740ba4b252edb8132767d1a031504f341d9aecf1
Sam Fuchs <samcfuchs@gmail.com>
2019-03-06 15:13:30 -0500

    Use tbapy bindings

--- scripts/EventRanking.py
+++ scripts/EventRanking.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 import time
 import sys
 import lib
+import tbapy
 
 """
 Generates the current ranking table for the given event
@@ -20,63 +21,49 @@ if int(YEAR) < 2007:
     raise ValueError("Only valid for 2007 onward")
 FILENAME = "data/Ranking_" + EVENT_KEY + ".csv"
 
-s = lib.init()
-
-def get_data():
-    print("  Getting rankings")
-    rank = s.get('{base}/event/{eventkey}/rankings'.format(base=lib.TBA_BASE, eventkey=EVENT_KEY))
-    print("  Getting oprs")
-    oprs = s.get('{base}/event/{eventkey}/oprs'.format(base=lib.TBA_BASE, eventkey=EVENT_KEY))
-    # Error if data is no good
-    if rank.status_code == 404 or rank.json()['rankings'] == None:
-        raise ValueError("Event data does not exist")
-
-    sort_orders = [x['name'] for x in rank.json()['sort_order_info']]
-    # 2018 data has an unlabeled empty field that is always 0s
-    # It's not in the sort order info so we add a label for that field
-    if YEAR == '2018':
-        sort_orders.append("Null")
-
-    return rank, oprs, sort_orders
+s, tba, _, _ = lib.init()
 
 
-def get_header(sort_orders):
+def get_header(rank):
     data = ""
     data += 'Rank,Team,'
-    data += ','.join(sort_orders)
+    data += ','.join(map(lambda p : p['name'], rank.sort_order_info))
+
+    if YEAR == "2018":
+        data += ",Null"
+
     data += ',W,L,T,OPR,DPR,CCWM\n'
     return data
 
 
-def get_team_data(team, oprs):
-    data = ""
-    data += str(team['rank']) + ','
-    data += team['team_key'][3:] + ','
-
-    for value in team['sort_orders']:
-        data += str(value) + ','
-
-    data += str(team['record']['wins']) + ','
-    data += str(team['record']['losses']) + ','
-    data += str(team['record']['ties']) + ','
-
-    data += str(oprs.json()['oprs'][team['team_key']]) + ','
-    data += str(oprs.json()['dprs'][team['team_key']]) + ','
-    data += str(oprs.json()['ccwms'][team['team_key']]) + ','
-
-    return data + "\n"
-
-
 print("Retrieving data")
-rank, oprs, SORT_ORDERS = get_data()
+rank = tba.event_rankings(EVENT_KEY)
+oprs = tba.event_oprs(EVENT_KEY)
 
 print("Writing file")
 with open(FILENAME, 'w') as f:
-    f.write(get_header(SORT_ORDERS))
+    f.write(get_header(rank))
 
     data = ""
-    for team in rank.json()['rankings']:
-        data += get_team_data(team, oprs)
+
+    for i in range(len(rank.rankings)):
+        team = rank.rankings[i]
+        team_key = team['team_key']
+        
+        data += str(team['rank']) + ','
+        data += str(team_key[3:]) + ','
+        data += ','.join(map(str, team['sort_orders'])) + ','
+
+        data += str(team['record']['wins']) + ','
+        data += str(team['record']['losses']) + ','
+        data += str(team['record']['ties']) + ','
+
+        data += str(oprs['oprs'][team_key]) + ','
+        data += str(oprs['dprs'][team_key]) + ','
+        data += str(oprs['ccwms'][team_key]) + ','
+
+        data += "\n"
+
     f.write(data)
 
 print("Wrote data to {0}".format(FILENAME))
