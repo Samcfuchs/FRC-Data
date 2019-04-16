2992071fa1057a038e3a04743bb6bf56fdadcde0
Sam Fuchs <samcfuchs@gmail.com>
2019-04-16 10:21:59 -0400

    Refactor TeamInfo.py
    - Use f-strings
    - Use tbapy
    - Use internal methods

--- scripts/TeamInfo.py
+++ scripts/TeamInfo.py
@@ -2,8 +2,9 @@ import requests
 from datetime import date, datetime
 import time
 # Make sure you set your API key first
-import geocoder
 import lib
+s, tba, has_tba, has_google = lib.init()
+import geocoder
 
 """
 Use TBA APIv3 to retrieve a list of current FRC teams and their data:
@@ -22,80 +23,69 @@ Store this data in a csv file TeamInfo.csv
 LINK_BASE = "https://www.thebluealliance.com/team/"
 FILENAME = 'data/TeamInfo.csv'
 
-s, has_tba, has_google = lib.init()
 
 # Get list of teams from TBA
 print("Getting data")
-teams = lib.get_team_data(0, '/simple', True)
+teams = tba.teams(simple=True)
 
-print("Retrieved {} teams".format(len(teams)))
+print(f"Retrieved {len(teams)} teams")
 
 problemTeams = []
 
-def get_coords(team_key):
-    r = s.get("{base}/team/{key}/simple".format(base=lib.TBA_BASE, key=team_key))
-    team = r.json()
-    print(team)
-
-    loc = ",".join([team['city'], team['state_prov'], team['country']])
+def get_coords(team):
+    loc = f"{team.city},{team.state_prov},{team.country}"
     geo = geocoder.google(loc, rate_limit=False)
-    print(geo)
     if (geo.status == "ZERO_RESULTS"):
-        print("No geocode for {} @ {}".format(team['key'], loc))
+        print("No geocode for {team.key} @ {loc}")
+        return "null,null", True
     elif (geo.status == "OVER_QUERY_LIMIT"):
-        print("Over query limit on {} @ {}".format(team['key'], loc))
+        print("Over query limit on {team.key} @ {loc}")
+        return "null,null", True
+    elif geo.status == "REQUEST_DENIED":
+        print("Google key not loaded")
+        return "null,null", True
     elif (geo.status == "OK"):
-        return geo
+        return ','.join(map(str,geo.latlng)), False
 
 print("Writing file")
 with open(FILENAME, 'w', encoding='utf-8') as f:
     f.write('Team,Nickname,City,State,Country,Latitude,Longitude\n')
     i = 0
     for team in teams[i:]:
-        print("{}: {}".format(i, team['key']))
+        print(f"{i}: {team.key}")
         i += 1
 
         if lib.is_team_historic(team):
-            f.write("{n},{nick},null,null,null,null,null\n".format(n=team['team_number'], nick=team['nickname']))
+            f.write(f"{team.team_number},{team.nickname},null,null,null,null,null\n")
             continue
 
-        f.write(str(team['team_number']))
-        f.write(',')
+        f.write(f'{team.team_number},')
 
         try:
-            f.write('"' + team['nickname'] + '"')
+            f.write(f'"{team.nickname}",')
         except UnicodeEncodeError:
-            print("Nickname: " + team['key'] + ": " + LINK_BASE + team['key'][3:])
-            problemTeams.append(team['key'])
-            f.write('null')
-        f.write(',')
+            print(f"Nickname: {team['key']}: {LINK_BASE}{team.key[3:]}")
+            problemTeams.append(team.team_number)
+            f.write('null,')
 
         try:
-            f.write('"{}"'.format(team['city']))
+            f.write(f'"{team.city}",')
         except UnicodeEncodeError:
-            print("City: " + team['key'] + ": " + LINK_BASE + team['key'][3:])
-            problemTeams.append(team['key'])
-            f.write('null')
-        f.write(',')
+            print(f"City: {team.key}: {LINK_BASE}{team.key[3:]}")
+            problemTeams.append(team.team_number)
+            f.write('null,')
 
-        f.write('"{}",'.format(team['state_prov']))
+        f.write(f'"{team.state_prov}",')
 
-        f.write('"{}",'.format(team['country']))
+        f.write(f'"{team.country}",')
 
-        if has_google != "":
-            loc = ",".join([team['city'], team['state_prov'], team['country']])
-            geo = geocoder.google(loc, rate_limit=False)
-            if (geo.status == "ZERO_RESULTS"):
-                print("No geocode for {} @ {}".format(team['key'], loc))
-            if (geo.status == "OK"):
-                f.write(str(geo.latlng[0]))
-                f.write(',')
-
-                f.write(str(geo.latlng[1]))
-            else:
-                problemTeams.append(team['key'])
-                print("Geocode error for {} @ {}".format(team['key'], loc))
-                f.write("null,null")
+        if has_google:
+            s,r = get_coords(team)
+            f.write(s)
+            if r:
+                problemTeams.append(team.team_number)
+        else:
+            f.write("null,null")
 
         f.write('\n')
 
