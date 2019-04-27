e229946a8a81c33df60aede0b9579283d07ef6eb
Sam Fuchs <samcfuchs@gmail.com>
2019-04-27 18:44:26 -0400

    Move trueskill model to module

---
+++ Analytics/frc_trueskill.py
@@ -0,0 +1,93 @@
+import trueskill as ts
+import pandas as pd
+import numpy as np
+from typing import Tuple
+import math
+import tbapy
+import os
+
+Alliance = Tuple[int, int, int]
+
+class TSModel:
+    table = pd.DataFrame(columns=['Team','Rating','Score','Rank'])
+    tba = tbapy.TBA(os.environ['TBA_API_KEY'])
+
+    def __init__(self, teams=[], tie_rate=0.02):
+        self.env = ts.setup(draw_probability=tie_rate)
+
+        self.table.Team = teams
+        self.table.Rating = [ts.Rating()] * len(self.table)
+        self.table.set_index('Team',inplace=True)
+
+
+    def rate(self, team):
+        """ Get the ranking information for a team or list of teams """
+        return self.table.loc[team,:]
+    
+
+    def rate_alliance(self, alliance:Alliance):
+        ratings = list(self.table.loc[alliance, 'Rating'])
+
+        mu = sum(r.mu for r in ratings)
+        sigma = sum((self.env.beta**2 + r.sigma**2) for r in ratings)
+
+        return ts.Rating(mu, sigma)
+
+    
+    def rating(self, team):
+        """ Get the specific rating for a team """
+        return self.table.loc[team,:]
+    
+
+    def empty_ratings(self):
+        """ Assign default ratings to all teams """
+        self.table.Rating = [ts.Rating()]*len(self.table)
+    
+
+    def train(self, row):
+        """ Train the model on a single match record """
+        r_blue = list(self.table.loc[row.blue, 'Rating'])
+        r_red = list(self.table.loc[row.red, 'Rating'])
+
+        result = { 'blue':1, 'red':1 }
+        if row.winner != 'tie':
+            result[row.winner] -= 1
+        
+        new_blue, new_red = ts.rate([r_blue, r_red], result.values())
+
+        self.table.loc[row.blue, 'Rating'] = new_blue
+        self.table.loc[row.red, 'Rating'] = new_red
+
+        return new_blue, new_red
+    
+
+    def predict(self, blue:Alliance, red:Alliance) -> float:
+        """ Predict the outcome of a match """
+        r_blue = list(self.table.loc[blue, 'Rating'])
+        r_red = list(self.table.loc[red, 'Rating'])
+
+        blue_mu = sum(r.mu for r in r_blue)
+        blue_sigma = sum((self.env.beta**2 + r.sigma**2) for r in r_blue)
+
+        red_mu = sum(r.mu for r in r_red)
+        red_sigma = sum((self.env.beta**2 + r.sigma**2) for r in r_red)
+
+        x = (blue_mu - red_mu) / math.sqrt(blue_sigma+red_sigma)
+        p_blue_win = self.env.cdf(x)
+        return p_blue_win
+    
+
+    def quality(self, blue:Alliance, red:Alliance) -> float:
+        """ Get the generalized quality of a match """
+        r_blue = list(self.table.loc[blue, 'Rating'])
+        r_red = list(self.table.loc[red, 'Rating'])
+
+        return ts.quality((r_blue,r_red))
+    
+    def score(self):
+        self.table.Score = list(map(self.env.expose, self.table.Rating))
+    
+
+    def rank(self):
+        self.score()
+        self.table.Rank = self.table.Score.rank(ascending=False)
