3859d3ce74d5fe614287295383a199c96fc07077
Sam Fuchs <samcfuchs@gmail.com>
2019-06-22 14:25:32 -0400

    Remove pipeline from trueskill notebook

--- Analytics/trueskill_demo.py
+++ Analytics/trueskill_demo.py
@@ -16,65 +16,6 @@ import os
 
 tba = tbapy.TBA(os.environ['TBA_API_KEY'])
 
-#%%
-# Create our data processing pipeline
-def process_data(df):
-    """
-    Collate teams into 3-tuple alliances for each match.
-    """
-    cols_ren = {
-        'Competition Level':'comp_level',
-        'Match Number': 'match',
-        'Set Number': 'set',
-    }
-    team_cols = ['blue1','blue2','blue3','red1','red2','red3']
-
-    df.rename(columns=cols_ren, inplace=True)
-
-    df.drop(['City','State','Country','Time'], axis=1, inplace=True)
-    df.winner.fillna('tie', inplace=True)
-    df.dropna(inplace=True)
-    df.red3 = pd.to_numeric(df.red3) # For 2006
-
-    df['blue'] = list(zip(df.blue1, df.blue2, df.blue3))
-    df['red'] = list(zip(df.red1, df.red2, df.red3))
-    df.drop(team_cols, axis=1, inplace=True)
-
-    df = df.loc[df['blue score'] + df['red score'] > -2,:]
-
-    df.reset_index(drop=True, inplace=True)
-
-    return df
-
-def sort_data(df):
-    """
-    Sort matches into the approximate order they occurred
-    """
-    sort_order = ['Week','event_n','Event','comp_level_n','set','match']
-
-    comp_level_f = { 'qm':0, 'qf':1, 'sf':2, 'f':3 }
-    event_f = lambda k: 1 if k[:3] == 'cmp' else 0
-
-    df['comp_level_n'] = df.comp_level.map(comp_level_f)
-    df['event_n'] = df.Event.map(event_f)
-
-    df.sort_values(sort_order, inplace=True)
-    df.reset_index(inplace=True, drop=True)
-
-    df.drop(['event_n','comp_level_n'], axis=1, inplace=True)
-
-    return df
-
-def get_teams(years):
-    """
-    Get a list of all teams that competed in the given range of years
-    """
-    teams = set()
-    for year in years:
-        teams.update([int(key[3:]) for key in tba.teams(year=year, keys=True)])
-    
-    return list(teams)
-
 #%% [markdown]
 # ## Training the model
 # We can now train the model on a range of years. I've already built the
@@ -85,7 +26,7 @@ def get_teams(years):
 #%%
 # Multi-year simulation
 years = range(2005, 2020)
-teams = get_teams(years)
+teams = models.get_teams(years)
 model = TSModel(teams, logging=True)
 
 print(f"Training on {','.join(map(str, years))}")
@@ -128,8 +69,8 @@ trainedmodel = TSModel(logging=True)
 trainedmodel.load(f"{YEAR-1}_end_ratings.csv")
 
 data = pd.read_csv(f"../data/{YEAR}_MatchData_ol.csv")
-data = process_data(data)
-data = sort_data(data)
+data = models.process_data(data)
+data = models.sort_data(data)
 
 print(f"Year: {YEAR}")
 print(f"Simulating {len(data)} matches")
