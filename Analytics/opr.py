4479e992e84633e56a3d6ea4f3d7a413f3b3dd6a
Sam Fuchs <samcfuchs@gmail.com>
2020-02-28 17:25:58 -0500

    Use fast OPR calculation

--- Analytics/opr.py
+++ Analytics/opr.py
@@ -47,9 +47,11 @@ red['alliance'] = ['red']*len(red)
 red.rename(columns=COLS_REN, inplace=True)
 red.index = red.index * 2 + 1
 
+# Shuffle data back together
 data = pd.concat([blue,red], axis=0).sort_index()
 data = data[['key','alliance','teams','score']]
 
+# Get a list of teams
 teams = list(set([t for a in data.teams for t in a]))
 
 data.head(10)
@@ -62,51 +64,13 @@ data.head(10)
 # teams and rows, which are alliance-matches. Each row has a 1 for each team
 # that competed in that alliance-match.
 
-#%%
-# Build sparse matrix
-sparse = pd.DataFrame(0, index=np.arange(len(data)), columns=['key','alliance','score']+teams)
-sparse['key'] = data['key']
-sparse['alliance'] = data['alliance']
-sparse['score'] = data['score']
-
-#%% [markdown] 
-# Here we populate the matrix with 1's everywhere that a team participated in
-# the match in question. This is the step that takes the longest--we should try
-# to optimize this more if possible.
-
-#%%
-# Populate matrix
-def f(row):
-    sparse.loc[(sparse.key==row.key) & (sparse.alliance==row.alliance), row.teams] = 1
-
-start = time.time()
-data.apply(f, axis=1)
-print(f"Time: {int(time.time() - start)} s")
-
-# %% [markdown]
-# Having built our data structures, we simply use numpy's least-squares
-# algorithm to solve the system with the sparse matrix and score column.
-
-#%%
-# Solve for OPRs
-coef = sparse.drop(['key','alliance','score'], axis=1).to_numpy()
-oprs,resid,_,_ = np.linalg.lstsq(coef, data.score, rcond=None)
-
-#%%
-# Display OPRs
-opr_dict = { t:o for (t,o) in zip(teams, oprs) }
-
-opr_table = pd.DataFrame({'team':teams, 'opr':oprs})
-opr_table.sort_values('opr', ascending=False, inplace=True)
-opr_table.head()
-
 # %% [markdown]
 # ## Optimization
 #
 # But we can optimize this process a lot! Populating the sparse matrix is slowed
-# downsignificantly by the fact that we're doing it on a pandas dataframe. If we
-# work with a dictionary instead, we can use the `update` function to speed up
-# our indexing by orders of magnitude.
+# down significantly by the fact that we're doing it on a pandas dataframe. If
+# we work with a dictionary instead, we can use the `update` function to speed
+# up our indexing by orders of magnitude.
 #
 # First we'll build an empty matrix. This is a dictionary holding a lot of
 # dictionaries, the first axis being the match-alliance id and the second being
@@ -116,28 +80,49 @@ opr_table.head()
 # %%
 # Build sparse matrix
 row = {t:0 for t in teams}
-sparse_0 = {f"{r.key}_{r.alliance[0]}":dict(row) for i,r in data.iterrows()}
+sparse = {f"{r.key}_{r.alliance[0]}":dict(row) for i,r in data.iterrows()}
 
-# %%
+# %% [markdown]
 # Now we'll iterate over the dataframe again, but this time we'll use the faster
 # indexing function of the dictionary instead of slow pandas indexing to insert
 # values back into the sparse matrix.
 
+#%% [markdown] 
+# Here we populate the matrix with 1's everywhere that a team participated in
+# the match in question. This is the step that takes the longest--we should try
+# to optimize this more if possible.
+
 # %%
 # Populate matrix
 def build_sparse(row):
-    sparse_0[f"{row.key}_{row.alliance[0]}"].update(dict(zip(row.teams, [1,1,1])))
+    sparse[f"{row.key}_{row.alliance[0]}"].update(dict(zip(row.teams, [1,1,1])))
 
 data.apply(build_sparse, axis=1)
 
+# %% [markdown]
+# In order to perform numpy operations, we need to convert this layered
+# dictionary back into a numpy array.
+
 # %%
-# Deconstruct sparse into matrix
-sparse_m = np.array([list(d.values()) for d in sparse_0.values()])
+# Deconstruct sparse dictionary into matrix
+sparse_m = np.array([list(d.values()) for d in sparse.values()])
 sparse_m
+
+# %% [markdown]
+# Having built our data structures, we simply use numpy's least-squares
+# algorithm to solve the system with the sparse matrix and score column.
 
 # %%
 # Solve the matrix
 oprs,resid,_,_ = np.linalg.lstsq(sparse_m, data.score, rcond=None)
+
+#%%
+# Display OPRs
+opr_dict = { t:o for (t,o) in zip(teams, oprs) }
+
+opr_table = pd.DataFrame({'team':teams, 'opr':oprs})
+opr_table.sort_values('opr', ascending=False, inplace=True)
+opr_table.head()
 
 #%%
 # Visualize OPRs
@@ -184,6 +169,7 @@ plt.show()
 # %%
 print("Median (absolute) error:", np.sqrt(np.square(residuals).median()))
 
-# %% [markdown]
+# %% [markdown] 
 # This makes it look like our prediction model is probably a bit more accurate,
-# except for when teams have standout performances.
+# although it's influenced by outliers--essentially, it can't predict when teams
+# have standout performances.
