65761f5da9c46473daab69c3cac7b44745e11fd1
Sam Fuchs <samcfuchs@gmail.com>
2020-03-04 03:47:45 -0500

    Demo solutions for multiple variables

--- Analytics/2020_accuracy.py
+++ Analytics/2020_accuracy.py
@@ -6,28 +6,31 @@ import numpy as np
 import pandas as pd
 
 # %%
+targets = ['teleopCellsInner','teleopCellsOuter','teleopCellsBottom', 'autoCellPoints', 'rp']
+
 matchdata = pd.read_csv("../data/2020_MatchData.csv", index_col=[0,12])
 matchdata_ol = pd.read_csv("../data/2020_MatchData_ol.csv", index_col=0)
 
 teams, team_data = OPRModel.load(matchdata_ol.copy())
-matchdata = matchdata.loc[matchdata["Robot Number"]==1,['teleopCellsInner','teleopCellsOuter']]
+matchdata = matchdata.loc[matchdata["Robot Number"]==1,targets]
 
 table = matchdata.merge(team_data['teams'], how='left', left_index=True, right_index=True)
 table.head()
 
 # %%
-innermodel = OPRModel()
-outermodel = OPRModel()
+model = OPRModel()
+y = table.drop('teams',axis=1).to_numpy()
+y_names = table.drop('teams',axis=1).columns
+y_names = {i:n for i,n in enumerate(y_names)}
 
-innermodel.train(table, table.teleopCellsInner)
-outermodel.train(table, table.teleopCellsOuter)
-
-combo = pd.DataFrame({'innerCellsContribution':innermodel.table.opr, 'outerCellsContribution':outermodel.table.opr})
-combo.head()
-
-# %%
-combo['output'] = combo.apply(lambda r: 3 * r.innerCellsContribution + 2 * r.outerCellsContribution, axis=1)
-combo.sort_values('output',ascending=False).head(10)
+model.train(table, y)
+model.table.rename(columns=y_names,inplace=True)
+results = model.table.copy()
+results.head(10)
 
 # %%
-combo.to_csv('data/2020_cell_contribution.csv')
+results['output'] = results.apply(lambda r: 3 * r.teleopCellsInner + 2 * r.teleopCellsOuter + r.teleopCellsBottom, axis=1)
+results.sort_values('output',ascending=False).head(10)
+
+# %%
+#combo.to_csv('data/2020_cell_contribution.csv')
